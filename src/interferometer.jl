887f882d9e67fe3fcee245defc42f3d27a29b9aa
Anchal Gupta <guptaa@fusion.gat.com>
2023-09-26 19:26:25 -0700

    Synthetic Interferometer calculation added
    Using QuadGK, a simple line integration of electron density is added.
    However, no test has been added and this has not been tested yet but
    the workflow would more or less remain teh same. Also note that for
    line average density, currently the total path length is used while
    we want to average by path length in plasma.

--- src/interferometer.jl
+++ src/interferometer.jl
@@ -1,5 +1,7 @@
 import PhysicalConstants.CODATA2018: c_0, ε_0, m_e, e
-import SD4SOLPS: fill_in_extrapolated_core_profile, check_rho_1d, add_rho_to_equilibrium
+import SD4SOLPS: fill_in_extrapolated_core_profile, check_rho_1d,
+    add_rho_to_equilibrium, core_profile_2d
+import QuadGK: quadgk
 
 default_ifo = "$(@__DIR__)/default_interferometer.yml"
 
@@ -37,7 +39,7 @@ Add interferometer to IMAS structure using a Dict and compute the line integrate
 electron density if not present
 """
 function add_interferometer(
-    @nospecialize(ids::OMAS.dd)=OMAS.dd(),
+    @nospecialize(ids::OMAS.dd),
     config::Dict{Symbol, Any},
 )::OMAS.dd
     OMAS.dict2imas(config, ids)
@@ -51,7 +53,7 @@ end
   - Calculate phase_to_n_e_line if not present for each wavelength
   - Compute the line integrated electron density if not present
 """
-function compute_interferometer(@nospecialize(ids::OMAS.dd))
+function compute_interferometer(@nospecialize(ids::OMAS.dd), rtol::Float64=1e-6)
     for ch ∈ ids.interferometer.channel
         k = [2 * π / ch.wavelength[ii].value for ii ∈ 1:2]
         for i1 ∈ 1:2
@@ -82,12 +84,56 @@ function compute_interferometer(@nospecialize(ids::OMAS.dd))
                     "core profile time slices",
                 )
             end
+            # Parametrize line of sight
+            if ch.line_of_sight.third_point ==
+               OMAS.interferometer__channel___line_of_sight__third_point()
+                function los(s::Float64)
+                    fp = ch.line_of_sight.first_point.r, ch.line_of_sight.first_point.z
+                    sp =
+                        ch.line_of_sight.second_point.r, ch.line_of_sight.second_point.z
+                    return fp .+ s .* (sp .- fp)
+                end
+            else
+                function los(s::Float64)
+                    fp = ch.line_of_sight.first_point.r, ch.line_of_sight.first_point.z
+                    sp =
+                        ch.line_of_sight.second_point.r, ch.line_of_sight.second_point.z
+                    tp = ch.line_of_sight.third_point.r, ch.line_of_sight.third_point.z
+                    if s <= 0.5
+                        return fp .+ 2 * s .* (sp .- fp)
+                    else
+                        return sp .+ (s - 0.5) .* (tp .- sp)
+                    end
+                end
+            end
+            # Derivative of length along line of sight with respect to s
+            dl_ds(s) =
+                if s <= 0.5
+                    return sqrt(sum((2 .* (sp .- fp)) .^ 2))
+                else
+                    return sqrt(sum((2 .* (sp .- tp)) .^ 2))
+                end
             fix_eq_time_idx = length(ids.equilibrium.time_slice) == 1
             for ii ∈ eachindex(epggd)
-                ch.n_e_line.time[ii] = epggd[ii].time
+                ch.n_e_line.time[ii] = ch.n_e_line_average.time[ii] = epggd[ii].time
                 eq_time_idx = fix_eq_time_idx ? 1 : ii
-                # WIP Integrate electron density along line of sight and add to value
-                ch.n_e_line.value[ii]
+                integrand(s) =
+                    core_profile_2d(
+                        ids,
+                        ii,
+                        eq_time_idx,
+                        "electrons.density",
+                        los(s)...,
+                    ) * dl_ds.(s)
+                ch.n_e_line.data[ii] = quadgk(integrand, 0, 1, rtol)[1]
+                # TO DO: Following line should divide by length in plasma (core) region
+                ch.n_e_line_average.data[ii] =
+                    ch.n_e_line.data[ii] / quadgk(dl_ds, 0, 1, rtol)[1]
+                for lam ∈ ch.wavelength
+                    lam.phase_corrected.time[ii] = epggd[ii].time
+                    lam.phase_corrected.data[ii] =
+                        ch.n_e_line.data[ii] / lam.phase_to_n_e_line
+                end
             end
         end
     end
