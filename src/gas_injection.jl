ee3784a12c893cb932b64fbf3fe1b15607f61400
Anchal Gupta <guptaa@fusion.gat.com>
2023-11-20 11:17:27 -0800

    Reached correct step response, but brute force
    At this commit, the correct step response can be regenerated from the
    model with appropriate dribbling model parameters. However, the
    response is created in a brute force implementation of using
    step response at every time step. This is not efficient and should be
    replaced with discrete signal processing using filter coefficients.
    
    Also, remove comments in the default_gas_injection.json file as Julia
    JSON parser does not support them yet.

--- src/gas_injection.jl
+++ src/gas_injection.jl
@@ -22,60 +22,60 @@ function compute_gas_injection!(ids::OMAS.dd, valves::Vector{Any})
     for vid ∈ eachindex(ids.gas_injection.valve)
         valve = ids.gas_injection.valve[vid]
         if length(valve.voltage.data) > length(valve.flow_rate.data)
-            start_ind = length(valve.flow_rate) + 1
-            end_ind = length(valve.voltage.command)
+            println("Computing flow rate for valve $(valve.name)")
+            start_ind = length(valve.flow_rate.data) + 1
+            end_ind = length(valve.voltage.data)
+            t = valve.voltage.time
+            cmd = valve.voltage.data
             if haskey(valves[vid], "dribbling_model")
                 dbm = valves[vid]["dribbling_model"]
+                println("Using dribbling_model")
                 if length(valve.flow_rate.data) == 0
                     # Initialize dribbling model state variables
-                    for key ∈ ["Gamma_spike", "Gamma_main", "Gamma_end"]
+                    for key ∈ ["Gamma_main"]
                         dbm[key] = []
                     end
-                    # for key ∈ ["t0", "tf"]
-                    #     dbm[key] = Inf
-                    # end
+                    valve.flow_rate.data = [0.0]
+                    valve.flow_rate.time = [t[1]]
+                    start_ind = 2
+                    dbm["t0"] = Inf
+                    Gamma_end = (t) -> 0.0
                 end
 
-                # if dbm["t0"] == Inf
-                #     for ii ∈ start_ind:end_ind
-                #         if valve.voltage.command[ii] > 0
-                #             dbm["t0"] = valve.voltage.time[ii] # First time valve opens
-                #             break
-                #         end
-                #     end
-                # end
-                # if dbm["tf"] == Inf && dbm["t0"] != Inf
-                #     for ii ∈ start_ind:end_ind
-                #         if valve.voltage.command[ii] == 0 && valve.voltage.time[ii] > dbm["t0"]
-                #             dbm["tf"] = valve.voltage.time[ii] # First time valve closes
-                #             break
-                #         end
-                #     end
-                # end
+                if dbm["t0"] == Inf
+                    for ii ∈ start_ind:end_ind
+                        if cmd[ii] > 0
+                            dbm["t0"] = valve.voltage.time[ii] # First time valve opens
+                            break
+                        end
+                    end
+                end
+                Gamma_spike =
+                    (t) ->
+                        if t > dbm["t0"] + dbm["delta_on"]
+                            dt = dbm["t0"] + dbm["delta_on"] - t
+                            return dbm["Gamma_s"] * exp(dt / dbm["tau_spike"])
+                        else
+                            return 0.0  # Do nothing before command time + on delay
+                        end
                 for ii ∈ start_ind:end_ind
+                    command_step = cmd[ii] - cmd[ii-1]
                     append!(
-                        dbm["Gamma_spike"],
-                        get_Gamma_spike(t[ii], valve.voltage.command[ii-1:ii], dbm),
+                        dbm["Gamma_main"],
+                        [get_Gamma_main(t[ii], command_step, dbm)],
                     )
-                    command_step = valve.voltage.data[ii] - valve.voltage.data[ii-1]
-                    append!(dbm["Gamma_main"], get_Gamma_main(t[ii], command_step, dbm))
-                    append!(
-                        dbm["Gamma_end"],
-                        get_Gamma_end(
-                            t[ii],
-                            valve.voltage.command[ii-1:ii],
-                            dbm,
-                            cumulate_Gamma(dbm["Gamma_main"]),
-                        ),
-                    )
-                    total_Gamma_spike = cumulate_Gamma(dbm["Gamma_spike"])
                     total_Gamma_main = cumulate_Gamma(dbm["Gamma_main"])
-                    total_Gamma_end = cumulate_Gamma(dbm["Gamma_end"])
+                    Gamma_end = update_Gamma_end(
+                        Gamma_end,
+                        t[ii],
+                        cmd[ii-1:ii],
+                        dbm)
                     append!(
                         valve.flow_rate.data,
-                        total_Gamma_spike(t[ii]) + total_Gamma_main(t[ii]) +
-                        total_Gamma_end(t[ii]),
+                        Gamma_spike(t[ii]) + total_Gamma_main(t[ii]) +
+                        Gamma_end(t[ii]),
                     )
+                    append!(valve.flow_rate.time, t[ii])
                 end
             elseif length(valve.response_curve.flow_rate) ==
                    length(valve.response_curve.voltage) > 1
@@ -86,40 +86,26 @@ function compute_gas_injection!(ids::OMAS.dd, valves::Vector{Any})
                 for ii ∈ start_ind:end_ind
                     append!(
                         valve.flow_rate.data,
-                        valve_response(valve.voltage.data[ii]),
+                        valve_response(cmd[ii]),
                     )
+                    append!(valve.flow_rate.time, t[ii])
                 end
             end
         end
     end
 end
 
-function get_Gamma_spike(command_time, command_recent_two, dbm)
-    if command_recent_two[1] == 0 && command_recent_two[2] > 0
-        Gamma_spike = (t) ->
-            if t > command_time + dbm["delta_on"]
-                dt = command_time + dbm["delta_on"] - t
-                return dbm["Gamma_s"] * exp(dt / dbm["tau_spike"])
-            else
-                return 0.0  # Do nothing before command time + on delay
-            end
-    else # Return identical zero if no turn on detected
-        Gamma_spike = (t) -> 0.0
-    end
-    return Gamma_spike
-end
-
 function instant_gas_model(command, config)
-    return config["p1"] .* (sqrt.(((command * config["p2"]) .^ 2.0 .+ 1) .- 1))
+    return config["p1"] .* (sqrt.(((command .* config["p2"]) .^ 2.0 .+ 1) .- 1))
 end
 
 function get_Gamma_main(command_time, command_step, dbm)
-    Gamma_0 = instant_gas_model(command_step, dbm)
+    Gamma_0 = instant_gas_model(command_step, dbm) * sign(command_step)
     function Gamma_main(t)
-        if t > command_time + dbm["delta_on"]
+        if t > command_time + dbm["delta_on"] && Gamma_0 != 0.0
             dt = command_time + dbm["delta_on"] - t
             decay_on = (1 - exp(dt / dbm["tau_on"]))
-            decay_over = (1 + dbm["Gamma_over_0"] / Gamma_0 * exp(dt / dbm["tau_over"]))
+            decay_over = (1 + dbm["Gamma_over_0"] * exp(dt / dbm["tau_over"]))
             return Gamma_0 * decay_on * decay_over
         else
             return 0.0 # Do nothing before command time + on delay
@@ -128,24 +114,32 @@ function get_Gamma_main(command_time, command_step, dbm)
     return Gamma_main
 end
 
-function get_Gamma_end(command_time, command_recent_two, dbm, cum_Gamma_main)
+function update_Gamma_end(
+    Gamma_end,
+    command_time,
+    command_recent_two,
+    dbm,
+)
     if command_recent_two[1] > 0 && command_recent_two[2] == 0
+        Gamma_main_array = copy(dbm["Gamma_main"])
+        cum_Gamma_main = cumulate_Gamma(Gamma_main_array)
         Gamma_end =
             (t) -> begin
                 start_main_val = cum_Gamma_main(command_time + dbm["delta_on"])
                 if t < command_time + dbm["delta_on"]
                     return 0.0 # Do nothing before command time + on delay
-                elseif t >= command_time + dbm["delta_on"] + dbm["D"] # End1 case
-                    last_end1_val = start_main_val * exp(-dbm["D"] / dbm["tau_spike"])
+                elseif t >= command_time + dbm["delta_on"] + dbm["D"] # End2 case
+                    last_end1_val = start_main_val * exp(-dbm["D"] / dbm["tau_off1"])
                     dt = command_time + dbm["delta_on"] + dbm["D"] - t
                     return last_end1_val * exp(dt / dbm["tau_off2"]) - cum_Gamma_main(t)
-                else # End2 case
+                else # End1 case
                     dt = command_time + dbm["delta_on"] - t
                     return start_main_val * exp(dt / dbm["tau_off1"]) -
                            cum_Gamma_main(t)
                 end
             end
-    else # Return identical zero if no turn off detected
+    elseif command_recent_two[2] > 0 && command_recent_two[1] == 0
+        # Return identical zero if valve is turned back on
         Gamma_end = (t) -> 0.0
     end
     return Gamma_end
