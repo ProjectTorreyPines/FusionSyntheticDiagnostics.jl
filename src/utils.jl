d4bd7ce349f61bb46c3b016249cf2d413c7f5666
Anchal Gupta <guptaa@fusion.gat.com>
2024-05-29 14:37:37 -0700

    Using single add_diagnostic! for all SDs
    Added function add_diagnostic! to src/utils.jl which removes redundancy
    in functions add_interferometer! and add_langmuir_probe! and would be
    useful in future of add_bolometer!. This common function provides the
    same functionality of optional overwriting of same channel names.

--- src/utils.jl
+++ src/utils.jl
@@ -4,6 +4,126 @@ end
 
 Base.showerror(io::IO, e::OverwriteAttemptError) = print(io, e.var)
 
+"""
+    add_diagnostic!(
+        config::String,
+        diagnostic::Symbol,
+        @nospecialize(ids::IMAS.dd)=IMAS.dd();
+        channel::Union{Symbol, Vector{Symbol}}=:channel,
+        overwrite=false,
+        verbose=false,
+    )::IMAS.dd
+
+Add a diagnostic to IMAS structure using a `JSON` file or Julia `Dict`.
+
+Input arguments:
+
+  - `config`: Diagnostic information in a `JSON` file or Julia `Dict`.
+  - 'diagnostic`: Diagnostic name as Symbol in IMAS. For example, `:interferometer`.
+  - `ids`: `IMAS.dd` where diagnostic will be added. If not provided, an empty
+    structure will be created to add the diagnostic to.
+  - `channel`: What different instances of diagnostic are called in IMAS. For example,
+    interferometer has :channel while langmuir_probes has :embedded and :reciprocating.
+  - `overwrite`: If true, even if config has a channel with same name or identifier as
+    one present in `ids`, it will be overwritten.
+  - `verbose`: Supplied to IMAS.dict2imas() function.
+"""
+function add_diagnostic!(
+    config::String,
+    diagnostic::Symbol,
+    @nospecialize(ids::IMAS.dd)=IMAS.dd();
+    channel::Union{Symbol, Vector{Symbol}}=:channel,
+    overwrite=false,
+    verbose=false,
+)::IMAS.dd
+    if endswith(config, ".json")
+        config_dict = convert_strings_to_symbols(IMAS.IMASdd.JSON.parsefile(config))
+        add_diagnostic!(
+            config_dict,
+            diagnostic,
+            ids;
+            channel=channel,
+            overwrite=overwrite,
+            verbose=verbose,
+        )
+    else
+        error("Only JSON files are supported.")
+    end
+    return ids
+end
+
+function add_diagnostic!(
+    config::Dict{Symbol, Any},
+    diagnostic::Symbol,
+    @nospecialize(ids::IMAS.dd)=IMAS.dd();
+    channel::Union{Symbol, Vector{Symbol}}=:channel,
+    overwrite=false,
+    verbose=false,
+)::IMAS.dd
+    if isa(channel, Vector)
+        for chan ∈ channel
+            add_diagnostic!(
+                config,
+                diagnostic,
+                ids;
+                channel=chan,
+                overwrite=overwrite,
+                verbose=verbose,
+            )
+        end
+        return ids
+    end
+    diag_name = String(diagnostic)
+    if !haskey(config, diagnostic)
+        warning("Config does not have " * diag_name * " in it. Skipping.")
+        return ids
+    end
+    ids_diag = getproperty(ids, diagnostic)
+    ids_diag_channels = getproperty(ids_diag, channel)
+    if length(ids_diag_channels) > 0
+        duplicate_indices = []
+        new_channels = Dict(
+            ch[:name] => ch[:identifier] for
+            ch ∈ config[diagnostic][channel]
+        )
+        for (ii, ch) ∈ enumerate(ids_diag_channels)
+            if ch.name in keys(new_channels) ||
+               ch.identifier in values(new_channels)
+                append!(duplicate_indices, ii)
+            end
+        end
+        if overwrite
+            for ii ∈ reverse(duplicate_indices)
+                println(
+                    "Overwriting " * diag_name * " channel ",
+                    "$(ids_diag_channels[ii].name)...",
+                )
+                deleteat!(ids_diag_channels, ii)
+            end
+        else
+            if length(duplicate_indices) > 0
+                err_msg =
+                    "Duplicate " * diag_name * " channels found with " *
+                    "overlapping names or identifiers.\n" * "Identifier: Name\n"
+                for ii ∈ duplicate_indices
+                    err_msg *=
+                        "$(ids_diag_channels[ii].identifier): " *
+                        "$(ids_diag_channels[ii].name)\n"
+                end
+                err_msg *= "Use overwrite=true to replace them."
+                throw(OverwriteAttemptError(err_msg))
+            end
+        end
+        config[diagnostic] = mergewith(
+            append!,
+            IMAS.imas2dict(ids_diag),
+            config[diagnostic],
+        )
+    end
+    IMAS.dict2imas(config, ids; verbose=verbose)
+    return ids
+end
+
 function convert_strings_to_symbols(d::Dict{String, Any})
     new_d = Dict{Symbol, Any}()
     for (k, v) ∈ d
