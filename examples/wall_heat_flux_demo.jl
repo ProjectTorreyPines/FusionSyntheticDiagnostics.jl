ef0363098053cebedbbe9834cbae956e90e4d37c
David Eldon <eldond@fusion.gat.com>
2024-07-30 11:39:40 -0700

    Add demonstration of calculating simple wall heat flux from IMAS tools

---
+++ examples/wall_heat_flux_demo.jl
@@ -0,0 +1,23 @@
+using SD4SOLPS: SD4SOLPS
+using IMAS: IMAS
+using Plots: Plots
+
+eqdsk_file = "geqdsk_iter_small_sample"
+sample_paths = [
+    splitdir(pathof(SD4SOLPS))[1] * "/../sample",
+]
+core_method = "simple"
+edge_method = "simple"
+filename = splitdir(pathof(SD4SOLPS))[1] * "/../sd_input_data"
+output_format = "json"
+dd = SD4SOLPS.preparation(
+    eqdsk_file,
+    sample_paths;
+    core_method=core_method,
+    filename=filename,
+    output_format=output_format,
+)
+
+Rwall, Zwall, rwall, zwall, s, SOL, r, q = IMAS.mesher_HF(dd)
+IMAS.particle_HF(dd.equilibrium.time_slice[1], SOL, rwall, zwall, r, q)
+Plots.plot(s, Qwall)
