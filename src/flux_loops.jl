00a61b96b626b86033a1e8dd79acdbcdd1546da9
Orso Meneghini <orso82@gmail.com>
2025-03-24 15:24:59 -0700

    simplified and formatted flux_loops and magnetic_probes routines

--- src/flux_loops.jl
+++ src/flux_loops.jl
@@ -1,73 +1,33 @@
 """
-    compute_flux_loops(PSI_interpolant::Interpolations.AbstractInterpolation,
-                                 loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}}) where T
+    compute_flux_loops!(
+        PSI_interpolant::Interpolations.AbstractInterpolation,
+        loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}},
+    ) where {T <: Real}
 
 Synthetic diagnostics for the flux loops saved in the data structure.
 Returns a vector with, for each loop, the measured poloidal flux.
 Also, updates the data structure with time and synthetic data. Data is saved at global time.
 """
+function compute_flux_loops!(
+    PSI_interpolant::Interpolations.AbstractInterpolation,
+    loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}},
+) where {T <: Real}
 
-function compute_flux_loops(PSI_interpolant::Interpolations.AbstractInterpolation,
-                                 loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}}) where T
-    @assert !isempty(loops) "Magnetic loops not inside the data structure. Impossible to create synthetic diagnostics"    
+    if isempty(loops)
+        return T[]
+    end
 
-    # get location of the loops 
-    psi = [PSI_interpolant(loop.position[1].r,loop.position[1].z) for loop in loops]
+    # get location of the loops
+    r = [loop.position.r for loop ∈ loops]
+    z = [loop.position.z for loop ∈ loops]
+
+    # psi at the loop positions
+    psi = PSI_interpolant.(r,z)
 
     # update dd with synthetic data
-    for (k,p) in enumerate(psi)
-        @ddtime(loops[k].flux.data = p)
+    for (k, p) ∈ enumerate(psi)
+        @ddtime(loops[k].flux.data = p[1] - p)
     end
-    return psi[1].-psi
 
-end
-
-
-"""
-    location_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-
-Returns the location of a flux loop saved in the data structure.
-"""
-
-function location_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-    @assert !isempty(loop) "Flux loop not inside the data structure. Impossible to retrieve position of loop"    
-    return (loop.position[1].r,loop.position[1].z)
-end
-
-function location_flux_loops(loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}}) where T
-    @assert !isempty(loops) "Flux loops not inside the data structure. Impossible to retrieve position of loops"    
-    return location_flux_loops.(loops)
-end
-
-
-"""
-    name_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-
-Returns the name of a flux loop saved in the data structure.
-"""
-function name_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-    @assert !isempty(loop) "Flux loops not inside the data structure. Impossible to retrieve name of loops"  
-    return loop.name
-end
-
-function name_flux_loops(loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}}) where T
-    @assert !isempty(loops) "Flux loops not inside the data structure. Impossible to retrieve name of loops"  
-    return name_flux_loops.(loops)
-end
-
-
-"""
-    data_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-
-Returns the (time, data) of a flux loop saved in the data structure.
-"""
-function data_flux_loops(loop::IMAS.magnetics__flux_loop{T}) where T
-    @assert !isempty(loop) "Flux loop not inside the data structure. Impossible to get data of loop"  
-    @assert !isempty(loop.flux.data) "No data is saved for the flux loop in the data structure"  
-    return (time = loop.flux.time, data = loop.flux.data)
-end
-
-function data_flux_loops(loops::IMAS.IDSvector{IMAS.magnetics__flux_loop{T}}) where T
-    @assert !isempty(loops) "Flux loops not inside the data structure. Impossible to get data of loop"  
-    return data_flux_loops.(loops)
+    return psi[1] .- psi
 end
--- src/magnetic_probes.jl
+++ src/magnetic_probes.jl
@@ -1,79 +1,37 @@
 """
-    compute_magnetic_probes(PSI_interpolant::Interpolations.AbstractInterpolation,
-                                 probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}}) where T
+    compute_magnetic_probes!(
+        PSI_interpolant::Interpolations.AbstractInterpolation,
+        probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}},
+    ) where {T<:Real}
 
 Synthetic diagnostics for the poloidal magnetic probes saved in the data structure.
 Returns a vector with, for each probe, the measured component of Bpol aligned with the probe.
 Also, updates the data structure with time and synthetic data. Data is saved at global time.
 """
+function compute_magnetic_probes!(
+    PSI_interpolant::Interpolations.AbstractInterpolation,
+    probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}},
+) where {T <: Real}
 
-function compute_magnetic_probes(PSI_interpolant::Interpolations.AbstractInterpolation,
-                                 probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}}) where T
-    @assert !isempty(probes) "Magnetic probes not inside the data structure. Impossible to create synthetic diagnostics"    
+    if isempty(probes)
+        return T[]
+    end
 
     # get location of the probes and their poloidal angle
-    r=[probe.position.r for probe in probes] 
-    z=[probe.position.z for probe in probes]
-    poloidal_angle = [probe.poloidal_angle for probe in probes] # clock-wise angle from the horizontal
+    r = [probe.position.r for probe ∈ probes]
+    z = [probe.position.z for probe ∈ probes]
+    poloidal_angle = [probe.poloidal_angle for probe ∈ probes] # clock-wise angle from the horizontal
 
-    Br, Bz = IMAS.Br_Bz(PSI_interpolant,r,z) # local Br and Bz at probe location
+    # local Br and Bz at probe location
+    Br, Bz = IMAS.Br_Bz(PSI_interpolant, r, z)
 
     #component of polodial field measured by the probe 
-    # B = n ̇Bp = Br cos(angle) - Bz sin(angle)
-    B = Br.*cos.(poloidal_angle) .- Bz.*sin.(poloidal_angle)
+    B = Br .* cos.(poloidal_angle) .- Bz .* sin.(poloidal_angle)
+
     # update dd with synthetic data
-    for (k,b) in enumerate(B)
+    for (k, b) ∈ enumerate(B)
         @ddtime(probes[k].field.data = b)
     end
+
     return B
-
-end
-
-
-"""
-    location_magnetic_probes(probe::IMAS.magnetics__b_field_pol_probe{T}) where T
-
-Returns the location of a poloidal magnetic probe saved in the data structure.
-"""
-
-function location_magnetic_probes(probe::IMAS.magnetics__b_field_pol_probe{T}) where T
-    @assert !isempty(probe) "Magnetic probe not inside the data structure. Impossible to retrieve position of probe"    
-    return (probe.position.r,probe.position.z)
-end
-
-function location_magnetic_probes(probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}}) where T
-    @assert !isempty(probes) "Magnetic probes not inside the data structure. Impossible to retrieve position of probes"    
-    return location_magnetic_probes.(probes)
-end
-
-
-"""
-    name_magnetic_probes(probes::IMAS.magnetics__b_field_pol_probe{T}) where T
-
-Returns the name of a poloidal magnetic probe saved in the data structure.
-"""
-function name_magnetic_probes(probe::IMAS.magnetics__b_field_pol_probe{T}) where T
-    @assert !isempty(probe) "Magnetic probes not inside the data structure. Impossible to retrieve name of probes"  
-    return probe.name 
-end
-
-function name_magnetic_probes(probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}}) where T
-    @assert !isempty(probes) "Magnetic probes not inside the data structure. Impossible to retrieve name of probes"  
-    return name_magnetic_probes.(probes)
-end
-
-"""
-    data_magnetic_probes(probe::IMAS.magnetics__b_field_pol_probe{T}) where T
-
-Returns the (time, data) of a poloidal magnetic probe saved in the data structure.
-"""
-function data_magnetic_probes(probe::IMAS.magnetics__b_field_pol_probe{T}) where T
-    @assert !isempty(probe) "Magnetic probe not inside the data structure. Impossible to get data of probe"  
-    @assert !isempty(probe.field.data) "No data is saved for the magentic probe in the data structure"  
-    return (time = probe.field.time, data = probe.field.data)
-end
-
-function data_magnetic_probes(probes::IMAS.IDSvector{IMAS.magnetics__b_field_pol_probe{T}}) where T
-    @assert !isempty(probes) "Magnetic probe not inside the data structure. Impossible to get data of probe"  
-    return data_magnetic_probes.(probes)
 end
